674fd2db74e512781cf168b27a29237130d60ccd
Vivek <vivek.rbj.up@gmail.com>
2023-11-03 03:05:58 +0530

    added mongo url

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const dotenv = require("dotenv");
+
 //1.1
 const mongoose = require("mongoose");
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+const port = process.env.PORT;
 
 mongoose.set('strictQuery', true);
 //1.2
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true}).then(db => console.log("connected to database")).catch(err => console.log(err));
+mongoose.connect(MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(db => console.log("connected to database")).catch(err => console.log(err));
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -111,6 +117,6 @@ app.get("/posts/:postId", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port " + port);
 });
